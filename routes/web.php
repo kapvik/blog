98e2c3a1f9b0c0a21d02af74c3d2053cb6a319d7
Vika Kapitulina <vika.kapitulina@gmail.com>
2018-05-30 20:47:59 +0300

    Create admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+		Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +21,8 @@ Route::get('/', function () {
 Route::get('/hello', function () {
     return view('hello');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
